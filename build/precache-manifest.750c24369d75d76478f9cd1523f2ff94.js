b7adf1505da52c7b74018e37445cc78760c29594
30496491 <j4m4n4@gmail.com>
2020-04-09 14:12:05 +0300

    second commit

---
+++ build/precache-manifest.750c24369d75d76478f9cd1523f2ff94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b799ea0e7fd6d1010a7faa0e41c9b0f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9181bd6fb784623c5377",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4c0cc097d386c69b1dcb",
+    "url": "/static/js/2.5333a41c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5333a41c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9181bd6fb784623c5377",
+    "url": "/static/js/main.923cb93d.chunk.js"
+  },
+  {
+    "revision": "f326c947c2bb131ebc6d",
+    "url": "/static/js/runtime-main.7900c9ab.js"
+  }
+]);
---
+++ build/precache-manifest.de16f968f8bd9c86746e47d9367da63a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3936d505734ba2eac5bd0631773aae35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1af5d5513091a224705",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4c0cc097d386c69b1dcb",
+    "url": "/static/js/2.5333a41c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5333a41c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1af5d5513091a224705",
+    "url": "/static/js/main.bb5d3844.chunk.js"
+  },
+  {
+    "revision": "f326c947c2bb131ebc6d",
+    "url": "/static/js/runtime-main.7900c9ab.js"
+  }
+]);
